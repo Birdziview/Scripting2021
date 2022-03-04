17e5d9b24aeffb43ea41885dc08caf64497f0e57
Jordan David <jordandillondavid@gmail.com>
2022-03-04 09:30:18 -0700

    1

---
+++ php/1_simple.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Simple PHP</title>
+</head>
+
+<body>
+	
+	<?php
+	
+	//define variables
+	$name="Jordan";
+	$eggs=42;
+	
+	//print content to the page
+	echo();
+	
+	?>
+	
+	
+</body>
+</html>
